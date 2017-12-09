4d116c998f473f36d1af40c0ed09d4a245382f85
Marco Gulino <marco@gulinux.net>
2017-12-09 16:09:35 +0000

    More generic configure/make system

---
+++ board.sh
@@ -0,0 +1,3 @@
+BOARD_FQBN="Arduino_STM32:STM32F1:mapleMini:bootloader_version=bootloader20,cpu_speed=speed_72mhz,opt=osstd"
+BOARD_VID_PID="0X1EAF_0X0004"
+#BOARD_FQBN="Arduino_STM32:STM32F1:mapleMini:bootloader_version=bootloader20,cpu_speed=speed_48mhz,opt=osstd"
