7f04552ebcb0caeef1d85b9dd8b40abc8d7baced
Marco Gulino <marco@gulinux.net>
2017-12-09 15:54:19 +0000

    Move files into common submodule

--- icons/generate_icons.sh
+++ icons/generate_icons.sh
@@ -4,5 +4,5 @@ cd "$WDIR"
 echo '#pragma once'cat >icons.h
 for file in *.png; do
     echo "Generating icon for $file" >&2
-    ./image2arduino.py -r 32 32 -o $( basename "$file" .png ) -p icon_ -b '#ffffff' -i $file >> icons.h
+    ../GuLinux-Commons/Arduino/tools/image2arduino.py -r 32 32 -o $( basename "$file" .png ) -p icon_ -b '#ffffff' -i $file >> icons.h
 done
